d36c57ce941141577c879111504d89fe8dc1b5b3
newspaperhobo <hoboinabox@gmail.com>
2023-03-25 17:48:58 -0400

    cleans up comments

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import { CityItem } from './components/CityItem';
-// Import and use the component in the App.js file
 import { InputField } from './components/shared/InputField';
 import React, { useState } from 'react';
 
@@ -36,23 +35,7 @@ function App() {
     },
   ];
 
-  /*
- // in the App.js file, import `useState` and using array destructuring, create a state that will hold the user input from the input field. This will be the city name user is supposed to enter.
-// Choose a name for your state and setter function to be descriptive.
-*/
-
   const [city, setCity] = useState("");
-
-  // const handleInputChange = (e) => {
-  //   console.log(e.target.value);
-  // };
-
-/*
-Create a handler function to update the state value that holds the city name
-  -Use handle word to start the name of the function it takes one parameter, event
-  -This handler function will take the value of the event target and set the state variable with the setter function
-  -Attach it to the input field with the proper attribute so that every time the user types something, this function is called
-*/
 
   const handleCityInput = e => {
     setCity(e.target.value)
--- src/components/CityItem.js
+++ src/components/CityItem.js
@@ -1,13 +1,6 @@
 import React from "react";
 
-export function CityItem({cities}) {
-
-  /*
--Create a handler function that will be called when this button is clicked
--This function will take a parameter. This parameter is the city it is clicked for.
--This function will only console log something so that we know it works as expected, for example the name of the city this buttons corresponds to.
-*/
-  
+export function CityItem({cities}) {  
   const handleClick = (city) => {
     console.log(city);
   };
@@ -17,9 +10,6 @@ export function CityItem({cities}) {
       <li>
         {city.EnglishName}, {city.AdministrativeArea.ID}, {city.Country.ID}
       </li>
-      {/* Add a button inside the ul element as a li element. */}
-      {/* This button will call the handler function every time it is clicked. Add the correct attributes to the button element to call the handler function when clicked. */}
-      {/* use anonymous function for onClick so handleclick function doesn't fire upon page loading */}
       <button onClick={() => handleClick(city.EnglishName)}>Log City</button>
     </ul>
   ));
